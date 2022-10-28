88be85181793bc24af304a4e63db0c9bc319c6da
Dilkhush309 <43719856+Dilkhush309@users.noreply.github.com>
2022-10-28 17:09:24 +0900

    Fixed the update button issue and add local storage.

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -2,7 +2,6 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
 import { ITask } from 'src/app/model/task';
-import { Todo } from "../../Todo";
 
 @Component({
   selector: 'app-todos',
@@ -10,104 +9,95 @@ import { Todo } from "../../Todo";
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-  localItem: string | null;
- // todos: Todo[];
+  localItemToDo: string | null;
+  localItemInProgress: string | null;
+  localItemDone: string | null;
   todoForm !: FormGroup;
-  tasks : ITask [] = [];
-  inprogress : ITask [] = [];
-  done : ITask [] = [];
+  tasks: ITask[] = [];
+  inprogress: ITask[] = [];
+  done: ITask[] = [];
   updateIndex !: any;
-  isEditEnabled : boolean = false ;
+  isEditEnabled: boolean = false;
 
-  constructor( private fb : FormBuilder) {
-    // this.localItem = localStorage.getItem("todos");
-    // if (this.localItem == null) {
-    //   this.todos = [];
-    // }
-    // else {
-    //   this.todos = JSON.parse(this.localItem);
-    // }
-    this.localItem = localStorage.getItem("tasks");
-    if (this.localItem == null) {
+  constructor(private fb: FormBuilder) {
+    this.localItemToDo = localStorage.getItem("tasks");
+    this.localItemInProgress = localStorage.getItem("inprogress");
+    this.localItemDone = localStorage.getItem("done");
+
+    if (this.localItemToDo == null) {
       this.tasks = [];
     }
     else {
-      this.tasks = JSON.parse(this.localItem);
+      this.tasks = JSON.parse(this.localItemToDo);
     }
+
+    if (this.localItemInProgress == null) {
+      this.inprogress = [];
+    }
+    else {
+      this.inprogress = JSON.parse(this.localItemInProgress);
+    }
+
+    if (this.localItemDone == null) {
+      this.done = [];
+    }
+    else {
+      this.done = JSON.parse(this.localItemDone);
+    }
+
   }
 
   ngOnInit(): void {
     this.todoForm = this.fb.group({
-      item : ['', Validators.required]
+      item: ['', Validators.required]
     })
   }
 
-  addTask(){
+  addTask() {
     this.tasks.push({
-      description:this.todoForm.value.item,
-      done:false,
+      description: this.todoForm.value.item,
+      done: false,
       editMode: false
     });
     localStorage.setItem("tasks", JSON.stringify(this.tasks));
     this.todoForm.reset();
   }
 
-  onEditTodoTask(item : ITask, i: number){
-    item.editMode = true
-    // this.todoForm.controls['item'].setValue(item.description);
-    // this.updateIndex = i;
-    // this.isEditEnabled = true;
+  onEditTodoTask(item: ITask, i: number) {
+    item.editMode = true;
     localStorage.setItem("tasks", JSON.stringify(this.tasks));
   }
 
-  updateTodoTask(f: NgForm, item: ITask) {
-  item.editMode = false
-  item.description = f.value.desc
-
-    // this.tasks[this.updateIndex].description = this.todoForm.value.item;
-    // this.tasks[this.updateIndex].done = false;
-    // this.todoForm.reset();
-    // this.updateIndex = undefined;
-    // this.isEditEnabled = false;
+  updateTodoTask(todoForm: NgForm, item: ITask) {
+    item.editMode = false
+    item.description = todoForm.value.desc
     localStorage.setItem("tasks", JSON.stringify(this.tasks));
   }
 
-  // onEditInProgressTask(item : ITask, i: number){
-  //   this.todoForm.controls['item'].setValue(item.description);
-  //   this.updateIndex = i;
-  //   this.isEditEnabled = true;
-  // }
-
-  // updateInProgressTask() {
-  //   console.log(this.inprogress)
-  //   this.inprogress[this.updateIndex].description = this.todoForm.value.item;
-  //   this.inprogress[this.updateIndex].done = false;
-  //   this.todoForm.reset();
-  //   this.updateIndex = undefined;
-  //   this.isEditEnabled = false;
-  // }
-
-  deleteTask(i: number){
-    this.tasks.splice(i,1);
+  deleteTask(i: number) {
+    this.tasks.splice(i, 1);
     localStorage.setItem("tasks", JSON.stringify(this.tasks));
 
   }
 
-  deleteInProgressTask(i: number){
-    this.inprogress.splice(i,1);
-    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+  deleteInProgressTask(i: number) {
+    this.inprogress.splice(i, 1);
+    localStorage.setItem("inprogress", JSON.stringify(this.inprogress));
 
   }
 
-  deleteDoneTask(i: number){
-    this.done.splice(i,1);
-    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+  deleteDoneTask(i: number) {
+    this.done.splice(i, 1);
+    localStorage.setItem("done", JSON.stringify(this.done));
   }
-
 
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      localStorage.setItem("inprogress", JSON.stringify(this.inprogress));
+      localStorage.setItem("done", JSON.stringify(this.done));
+
     } else {
       transferArrayItem(
         event.previousContainer.data,
@@ -115,24 +105,9 @@ export class TodosComponent implements OnInit {
         event.previousIndex,
         event.currentIndex,
       );
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      localStorage.setItem("inprogress", JSON.stringify(this.inprogress));
+      localStorage.setItem("done", JSON.stringify(this.done));
     }
   }
-
-  // deleteTodo(todo: Todo) {
-  //   console.log(todo);
-  //   const index = this.todos.indexOf(todo);
-  //   this.todos.splice(index, 1);
-  //   localStorage.setItem("todos", JSON.stringify(this.todos));
-  // }
-  // addTodo(todo: Todo) {
-  //   console.log(todo);
-  //   this.todos.push(todo);
-  //   localStorage.setItem("todos", JSON.stringify(this.todos));
-  // }
-  // toggleTodo(todo: Todo) {
-  //   console.log(todo);
-  //   const index = this.todos.indexOf(todo);
-  //   this.todos[index].active = !this.todos[index].active;
-  //   localStorage.setItem("todos", JSON.stringify(this.todos));
-  // }
 }
